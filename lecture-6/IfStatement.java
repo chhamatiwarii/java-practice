c851e30a720f71d4ee48fd4f1068219b1cff5c2e
Aman Sachan <codewithaman78@gmail.com>
2026-05-05 00:32:52 +0530

    add: if statement explanation

---
+++ lecture-6/IfStatement.java
@@ -0,0 +1,72 @@
+class Test {
+    public static void main(String[] args) {
+        // int x = 0;
+        // if(x) {                          // incompatible types
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Hi");
+        // }
+        // System.out.println("Kajal is slow");
+
+
+
+        // int x = 10;
+        // if(x = 20) {             // incompatible types: given expression doesn't resolve to boolean
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Hi");
+        // }
+        // System.out.println("Kajal is slow typer");
+
+
+        // int x = 10;
+        // if(x == 20) {             
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Hi");
+        // }
+        // System.out.println("Kajal is slow typer");
+
+
+        // boolean b = false;
+        // if(b = true) {
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Hi");
+        // }
+        // System.out.println("Kajal is lazy");
+
+        // boolean b = false;
+        // if(b == true) {
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Hi");
+        // }
+        // System.out.println("Kajal is lazy");
+
+        // Curly braces are optional in both if and else block 
+        // if there is only one statement
+        // but it should not be decalarative statement
+        // e.g: int x = 10;
+
+        // if(true) {
+        //     System.out.println("Hello, I am Kajal");
+        // }
+
+        // if(true);     // No output
+
+        // if(true)
+        //     int x = 10;   // error: declarative statement inside if without delimiters (curly braces)
+
+        // if(true) 
+        //     System.out.println("Hello");   // Statement dependent on if
+        // System.out.println("Hi");           // Statement independent of if
+        
+        // Interview Gotcha
+        // Is semicolon is a valid Java Statement?
+        // Semicolon is a valid Java Statement, we call it as empty statement it 
+        // doesn't provide any output
+
+
+    }
+}
--- lecture-6/Test.java
+++ lecture-6/Test.java
@@ -1,8 +1,11 @@
-class Test {
+public class Test {
+
     public static void main(String[] args) {
-        int x = 0;
-    if(x) {
-        System.out.println("");
-    }
+        // You need to take a user input
+        // if user input lies in range 0-20 then print "Very Low"
+        // if user input lies in range 21-40 then print "Low"
+        // if user input lies in range 41-60 then print "Medium"
+        // if user input lies in range 60-80 then print "High"
+        // if user input lies in range >80 then print "Highest"
     }
 }
