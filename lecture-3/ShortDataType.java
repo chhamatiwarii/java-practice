d4eaac02f708aeb7b3437881b213519401238928
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-05 22:17:23 +0530

    Added the flow control new program

--- lecture-3/ShortDataType.java
+++ lecture-3/ShortDataType.java
@@ -7,9 +7,9 @@ class Test {
 
         short s = 130;
         short s2 = 10;
-        short s3 = 204.7;  // possible lossy conversion from double to short
-        short s4 = 32768;  // possible lossy conversion from int to short
-        short s5 = true;   // incompatible types
+        // short s3 = 204.7;  // possible lossy conversion from double to short
+        // short s4 = 32768;  // possible lossy conversion from int to short
+        // short s5 = true;   // incompatible types
 
         // Short data type is best suitable for 16 bit processors like 8086
         // but these processors are completely outdated and hence the
--- lecture-6/Test.java
+++ lecture-6/Test.java
@@ -47,8 +47,26 @@ Edge Cases:
 - n = 5 -> 1 2 Fizz 4 Buzz
 */
 
+import java.util.Scanner;
+
 class Test {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Number = ");
+        int n = sc.nextInt();
+
+        if(n%3 == 0 && n%5 == 0){
+            System.out.println("FizzBuzz");
+        }else if(n%3 == 0){
+            System.out.println("Fizz");
+        }else if(n%5 == 0){
+            System.out.println("Buzz");
+        }else {
+            System.out.println(n);
+        }
+        System.out.println("\n PROGRAM END ‼️");
+
+    
         
     }
 }
