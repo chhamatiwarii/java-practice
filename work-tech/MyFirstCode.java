9d4ade2db7513cb9a7c3e590970221675b5ea7dc
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-05 23:33:15 +0530

    Added few more programs

---
+++ work-tech/MyFirstCode.java
@@ -0,0 +1,5 @@
+public class MyFirstCode {
+    public static void main(String[] args) {
+        System.out.println("My First code on workat.tech");
+    }
+}
---
+++ work-tech/SumUptoTen.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumUptoTen {
+    public static void main(String[] args) {
+        // int n =10;
+        // int sum = 0;
+        // for(int i=1 ; i<=n ; i++){
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Number = ");
+        int n = sc.nextInt();
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+    }
+}
