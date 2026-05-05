008a20e3d380ec59b4b7505afcddec2dc25d2de4
Aman Sachan <codewithaman78@gmail.com>
2026-05-06 00:40:26 +0530

    add: Check prime assignment problem

---
+++ assignments/PrimeCheck.java
@@ -0,0 +1,18 @@
+package assignments;
+
+/*
+    You need to take an integer from the user and check 
+    whether that number is prime or not, if the number is prime 
+    then print "Prime" and if not then print "Not Prime".
+
+    Make sure you write the most optimal solution.
+    (Optimized trial division)
+
+*/
+
+public class PrimeCheck {
+    
+    public static void main(String[] args) {
+        
+    }
+}
