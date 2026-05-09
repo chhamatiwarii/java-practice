4f1ae2916a9a700d29dbb4bbd4e40e116485762e
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-09 18:03:18 +0530

    Added new program

---
+++ work-tech/AddOne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Addone {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name = ");
+        String name = sc.nextLine();
+        System.out.print("Enter Your Age = ");
+        int a = sc.nextInt();
+        int b = a + 1;
+
+        System.out.println("Hello " +  name + "!" +  " Next year, you will be " +  b + " years old");
+        
+sc.close();
+    }
+}
