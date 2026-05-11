0acd87274c864671e8344f341274bc5e2d6fc89f
Aman Sachan <codewithaman78@gmail.com>
2026-05-11 22:58:47 +0530

    modify: odd even program assignment question

--- work-tech/OddEven.java
+++ work-tech/OddEven.java
@@ -5,19 +5,11 @@ public class OddEven {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your Number = ");
         int n = sc.nextInt();
-        boolean flag = false;
 
         for(int i = 1; i <= n; i++){
             int a = sc.nextInt();
-            if(a%2 == 0){
-                //System.out.println("Even");
-                flag = true; 
-                
-            }if(flag){
-                System.out.println("EVEN");
-            }else{
-                System.out.println("ODD");
-            }
+            if(a % 2 == 0) System.out.println("EVEN");
+            else System.out.println("ODD");
         }
         sc.close();
 
