a6f23bc2d106fbf298e27a77a8902eb7d7d0ea17
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-13 22:44:56 +0530

    palindrome program added

---
+++ work-tech/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter word = ");
+        String palindrome = sc.nextLine();
+
+        String palindromeReverse = "";
+        for(int i = palindrome.length()-1; i >= 0; i--){
+            palindromeReverse += palindrome.charAt(i);
+
+        }if(palindrome == palindromeReverse){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
