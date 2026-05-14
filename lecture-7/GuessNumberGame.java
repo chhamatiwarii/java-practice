c81166dc0bcf1ed6026e0d447ebd36db17a81312
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-14 22:33:09 +0530

    add: GuessNumberprogram

--- lecture-7/GuessNumberGame.java
+++ lecture-7/GuessNumberGame.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class GuessNumberGame {
     public static void main(String[] args) {
-        int randomNumber = (int)(Math.random() * 100 + 1);
+        int randomNumber = (int) (Math.random() * 100 + 1);
+        // System.out.println(randomNumber);
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 8; i++) {
+            System.out.print("Enter Your number = ");
+            int c = sc.nextInt();
+
+            if (randomNumber < c) {
+                System.out.println("Too High");
+            } else if (randomNumber > c) {
+                System.out.println("Too less");
+            } else if (randomNumber == c) {
+                System.out.println("You Guessed it right");
+                break;
+            }
+        }
+        if(randomNumber != c){
+            System.out.println("You lost" + " Correct Number is = " + randomNumber);
+        }
+
+        sc.close();
+        System.out.println("Program Ended");
     }
 }
