ca0af12346c814383a4b1bcd7aeb3031ceb9d51b
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-14 21:53:35 +0530

    add: While flow control program & fizzbuzz problem

---
+++ work-tech/Counting.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Counting {
+    public static void main(String[] args) {
+        // int c = 1;
+        // while(c<21){ 
+        //     if(c%2 == 0){    
+        //        System.out.println(c); 
+        //     }
+        //     c++;
+        // }
+        // System.out.println("Program Completed!");
+        //our code will run for 20 iterations, that's not feasible, because we want it to print 10 iterations out of 20
+        // other 10 iterations will get waste so this is not optimal solution to implement, there is scope to make it optimal
+
+        //2. Optimal solution 
+        // int i = 1;
+        // while(i<21){
+        //     System.out.println(i);
+        //     i += 2;
+        // }
+        // System.out.println("Program Ended !!");
+
+        //3. even
+        // int i = 2;
+        // while(i<21){
+        //     System.out.println(i);
+        //     i += 2;
+        // }
+        // System.out.println("Program Ended !!");
+
+        //4. Reverse
+        // int r = 50;
+        // while(r>0){
+        //     System.out.println(r);
+        //     r -= 5;
+
+        // }
+        // System.out.println("Program");
+
+    
+    }
+}
