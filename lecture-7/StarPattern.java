fdd75909023a249ccdb9dfd0adc558c6adb7e22a
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-15 00:04:15 +0530

    Add: new Starpattern program

--- lecture-7/StarPattern.java
+++ lecture-7/StarPattern.java
@@ -2,15 +2,55 @@ public class StarPattern {
     public static void main(String[] args) {
         int n = 5;
 
-        for(int i =1; i<=n; i++){
+        for(int i = 1; i<=n; i++){
             for(int j = 1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int k = 1; k<=i; k++){
+            for(int k =1; k<=i; k++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        for(int l =1; l<n; l++){
+            for(int m =1; m<=l; m++){
+                System.out.print(" ");
+            }
+            for(int o=1; o<=n-l; o++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // for(int i = 1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int k = 1; k<=n; k++){
+        //     for(int l = 1; l<=n-k; l++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for(int i = 1; i<=n; i++){
+        //     for(int k = 1; k<=n-i+1; k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i =1; i<=n; i++){
+        //     for(int j = 1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k = 1; k<=i; k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
 
