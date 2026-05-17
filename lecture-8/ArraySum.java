77020a40a6cbbca553b51d47b00e762ced3a8243
Chhama Tiwari <chhamatiwari830@gmail.com>
2026-05-17 18:20:23 +0530

    add: for and for each flow controller use for arraysum

--- lecture-8/ArraySum.java
+++ lecture-8/ArraySum.java
@@ -1,5 +1,18 @@
 class ArraySum {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        int m =0;
+        // for(int i =0; i<arr.length; i++){
+            
+        //     m = m + arr[i];
+        //     //System.out.println(arr[i]);
+        // }
+        // System.out.println(m);
+        
+
+        for (int k : arr) {
+            m += k; 
+        }
+        System.out.println(m);
     }
 }
