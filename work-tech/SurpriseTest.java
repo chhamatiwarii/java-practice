9f951e7dc8e9f4b4d795b3b6869ac34697be4dbf
Aman Sachan <codewithaman78@gmail.com>
2026-05-28 23:35:46 +0530

    add: sieve of eratoshthenes

--- work-tech/SurpriseTest.java
+++ work-tech/SurpriseTest.java
@@ -1,17 +1,35 @@
+import java.util.Arrays;
+
 public class SurpriseTest {
     public static void main(String[] args) {
-       int digit = 100;
-       
-       for(int i = 2; i<=100; i++){
-        boolean primeNumber = true;
-        for(int j = 2; j*j<=i; j++){
-            if(i%j == 0){
-                primeNumber = false;
+    //    outer: for(int i=2; i<=100; i++) {
+    //     boolean isPrime = true;
+    //     inner: for (int j = 2; j*j <= i; j++) {
+    //         if(i % j == 0) {
+    //             isPrime = false;
+    //             break inner;
+    //         }
+    //     }
+    //     if(isPrime) System.out.println(i);
+    //    }
+
+
+        // Sieve of eratosthenes
+        // when you need to perform some operations on the prime numbers in given range
+        boolean[] sieve = new boolean[10001];
+        Arrays.fill(sieve, true);
+
+        for(int i=2; i<=10000; i++) {
+            if(sieve[i] == true) {
+                for(int j= i * 2; j<=10000; j+=i) {
+                    sieve[j] = false;
+                }
             }
         }
-        if(primeNumber){
-            System.out.println(i);
+        int count = 0;
+        for(int i=2; i<=10000; i++) {
+            if(sieve[i]) count++;
         }
-       }
+        System.out.println(count);
     }
 }
